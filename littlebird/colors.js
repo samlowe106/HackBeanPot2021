820c3d97e0d5722552ee2f82a3cd749766486fe8
fhughes23 <hughes.f@northeastern.edu>
2021-02-20 17:40:43 -0500

    colors sheet

---
+++ littlebird/colors.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+    black : "#1A202C",
+    grey_darkest : "#718096",
+    grey_dark :  "#A0AEC0",
+    grey : "#CBD5E0",
+    grey_light : "#EDF2F7",
+    grey_lightest : "#F7FAFC",
+    white : "#FFFFFFF"
+}
